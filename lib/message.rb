8b20e69eba122e3a6a4a194e76731fbc554e375e
John Grubb <therealjohnnygrubb@gmail.com>
2012-07-26 13:08:56 -0400

    moved mailer model into /lib, added simple captcha

--- lib/message.rb
+++ lib/message.rb
@@ -4,11 +4,12 @@ class Message
   include ActiveModel::Conversion
   extend ActiveModel::Naming
 
-  attr_accessor :name, :email, :message
+  attr_accessor :name, :email, :message, :spamtrap
 
   validates :name, :email, :message, :presence => true
   validates :email, :format => { :with => %r{.+@.+\..+} }, :allow_blank => true
-  
+  validates :spamtrap, :format => {:with => /blue/, :message => "Go away until you know the answer"}
+
   def initialize(attributes = {})
     attributes.each do |name, value|
       send("#{name}=", value)
